a3ac5c9ec73e85275593e3c93a4083e6c84cf2d9
Joseph C <josephalexcobian@gmail.com>
2024-04-19 02:17:26 -0700

    <3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("test");
+import { Carousel } from "carousel-test-95";
+
+const carouselEl = document.querySelector(".carousel-outer");
+const carouselObj = new Carousel(carouselEl);
+
+const featuredLinkEl = document.querySelector("#featured-link");
+const menuLinkEl = document.querySelector("#menu-link");
+const aboutLinkEl = document.querySelector("#about-link");
+
+featuredLinkEl.addEventListener("click", () => {
+  carouselObj.jumpTo(1);
+});
+menuLinkEl.addEventListener("click", () => {
+  carouselObj.jumpTo(0);
+});
